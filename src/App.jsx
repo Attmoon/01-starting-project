0880b884d7990326e4563058dcc3b5c6feba473b
Attmoon <99157555+Attmoon@users.noreply.github.com>
2024-09-24 07:59:43 +0900

    조건적 contents 출력

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,8 @@ function App() {
     duration: 10
   });
 
+  const inputIsValid = userInput.duration >= 1;
+
   function handleChange(inputIdentifier, newValue) {
     setUserInput(prevUserInput => {
         return {
@@ -21,11 +23,14 @@ function App() {
     });
   }
 
+
+  
   return (
     <>
     <Header />
     <UserInput userInput={userInput} onChange={handleChange}/>
-    <Results input={userInput}/>
+    {!inputIsValid && <p className='center'>Please enter valid input data</p>}
+    {inputIsValid && <Results input={userInput}/>}
     </>
   );
 }
